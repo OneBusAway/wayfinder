990d2b9f4337414d149710bef8ee42b7508c7308
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-10-25 08:05:33 +0300

    feat: Add VehiclePopupContent component

---
+++ src/components/map/VehiclePopupContent.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let routeName;
+	export let vehicleId;
+	export let lastUpdated;
+	export let nextStopName;
+	export let status;
+</script>
+
+<div style="font-family: Arial, sans-serif; font-size: 14px;">
+	<div style="font-weight: bold;">{routeName}</div>
+	<div>
+		Vehicle #{vehicleId || 'N/A'} | Data updated {lastUpdated} seconds ago
+	</div>
+	<br />
+	<div style="font-weight: bold;">Next stop:</div>
+	<div style="padding-left: 20px;">
+		<strong>{nextStopName || 'N/A'}</strong>
+	</div>
+	<br />
+	<div style="font-weight: bold;">Status:</div>
+	<div style="padding-left: 20px;">
+		<strong>{status || 'N/A'}</strong>
+	</div>
+</div>
