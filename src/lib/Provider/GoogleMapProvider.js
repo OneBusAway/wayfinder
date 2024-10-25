d19fc8a0bb8fd4f377a2cbc2f8252fd9b0dbf93b
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-10-25 08:06:22 +0300

    feat: Add vehicle markers feature

--- src/lib/Provider/GoogleMapProvider.js
+++ src/lib/Provider/GoogleMapProvider.js
@@ -3,13 +3,16 @@ import StopMarker from '$components/map/StopMarker.svelte';
 import { faBus } from '@fortawesome/free-solid-svg-icons';
 import { COLORS } from '$lib/colors';
 import PopupContent from '$components/map/PopupContent.svelte';
+import VehiclePopupContent from '$components/map/VehiclePopupContent.svelte';
 export default class GoogleMapProvider {
 	constructor(apiKey) {
 		this.apiKey = apiKey;
 		this.map = null;
 		this.stopMarkers = [];
+		this.vehicleMarkers = [];
 		this.globalInfoWindow = null;
 		this.popupContentComponent = null;
+		this.stopsMap = new Map();
 	}
 
 	async initMap(element, options) {
@@ -97,6 +100,7 @@ export default class GoogleMapProvider {
 	}
 
 	addStopMarker(stop, stopTime = null) {
+
 		const marker = new google.maps.Marker({
 			position: { lat: stop.lat, lng: stop.lon },
 			map: this.map,
@@ -109,6 +113,8 @@ export default class GoogleMapProvider {
 				strokeColor: '#000000'
 			}
 		});
+
+		this.stopsMap.set(stop.id, stop);
 
 		marker.addListener('click', () => {
 			if (this.globalInfoWindow) {
@@ -144,6 +150,85 @@ export default class GoogleMapProvider {
 			marker.setMap(null);
 		});
 		this.stopMarkers = [];
+	}
+
+
+	addVehicleMarker(vehicle) {
+		if (!this.map) return null;
+
+		const icon = {
+			url: 'https://maps.google.com/mapfiles/kml/shapes/bus.png',
+			scaledSize: new google.maps.Size(30, 30),
+			anchor: new google.maps.Point(15, 15),
+			rotation: vehicle.orientation
+		};
+
+		const marker = new google.maps.Marker({
+			position: { lat: vehicle.position.lat, lng: vehicle.position.lon },
+			map: this.map,
+			icon: icon,
+			rotation: vehicle.orientation,
+			zIndex: 1000
+		});
+
+		this.vehicleMarkers.push(marker);
+
+		const nextStopName = this.stopsMap.get(vehicle.nextStop)?.name || 'N/A';
+
+		const popupContainer = document.createElement('div');
+
+		const popupComponent = new VehiclePopupContent({
+			target: popupContainer,
+			props: {
+				routeName: vehicle.routeName,
+				vehicleId: vehicle.vehicleId,
+				lastUpdated: vehicle.lastUpdated,
+				nextStopName: nextStopName,
+				status: vehicle.status
+			}
+		});
+
+		const infoWindow = new google.maps.InfoWindow({
+			content: popupContainer
+		});
+
+		marker.addListener('click', () => {
+			infoWindow.open(this.map, marker);
+		});
+
+		google.maps.event.addListener(infoWindow, 'closeclick', () => {
+			popupComponent.$destroy();
+		});
+
+		return marker;
+	}
+
+	updateVehicleMarker(marker, vehicleStatus) {
+
+		if (!this.map) return;
+
+		marker.setPosition({ lat: vehicleStatus.position.lat, lng: vehicleStatus.position.lon });
+	}
+
+	removeVehicleMarker(marker) {
+		marker.setMap(null);
+	}
+
+	centerAndZoomMarker(lat, lng) {
+		this.map.setCenter({ lat, lng });
+		this.map.setZoom(16);
+	}
+
+	clearVehicleMarkers() {
+		if (!this.map) return;
+
+		console.log(this.vehicleMarkers);
+		for (const marker of this.vehicleMarkers) {
+
+			console.log(marker);
+			marker.setMap(null);
+		}
+		this.vehicleMarkers = [];
 	}
 
 	cleanupInfoWindow() {
@@ -201,7 +286,7 @@ export default class GoogleMapProvider {
 			geodesic: true,
 			strokeColor: COLORS.POLYLINE,
 			strokeOpacity: 1.0,
-			strokeWeight: 4
+			strokeWeight: 1
 		};
 
 		if (addArrow) {
