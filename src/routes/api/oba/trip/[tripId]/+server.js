38a477902a239616f8c866a2524ffd05d6a548d2
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-10-26 05:22:08 +0300

    remove: unused url param

--- src/routes/api/oba/trip/[tripId]/+server.js
+++ src/routes/api/oba/trip/[tripId]/+server.js
@@ -1,6 +1,6 @@
 import oba, { handleOBAResponse } from '$lib/obaSdk';
 
-export async function GET({ params, url }) {
+export async function GET({ params}) {
 	const { tripId } = params;
 
 	const response = await oba.trip.retrieve(tripId);
