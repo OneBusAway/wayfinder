1c252e3b7644efd86b946db7fd0cb261ce9b5e21
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-10-26 13:09:11 +0300

    refactor: Update vehicle popup content styling and data display

--- src/components/map/VehiclePopupContent.svelte
+++ src/components/map/VehiclePopupContent.svelte
@@ -1,24 +1,46 @@
 <script>
 	export let routeName;
 	export let vehicleId;
-	export let lastUpdated;
+	export let lastUpdateTime;
 	export let nextStopName;
 	export let status;
+	export let predicted;
+
+	function formatLastUpdated(timestamp) {
+		const date = new Date(timestamp);
+		const now = new Date();
+		const secondsAgo = Math.floor((now - date) / 1000);
+
+		const minutes = Math.floor(secondsAgo / 60);
+		const seconds = secondsAgo % 60;
+
+		if (minutes > 0) {
+			return `${minutes} min${minutes > 1 ? 's' : ''} ${seconds} sec${seconds > 1 ? 's' : ''} ago`;
+		}
+		return `${seconds} sec${seconds > 1 ? 's' : ''} ago`;
+	}
 </script>
 
-<div style="font-family: Arial, sans-serif; font-size: 14px;">
-	<div style="font-weight: bold;">{routeName}</div>
-	<div>
-		Vehicle #{vehicleId || 'N/A'} | Data updated {lastUpdated} seconds ago
+<div class="max-w-xs rounded-lg bg-white p-4 shadow-md">
+	<div class="text-lg font-bold text-gray-800">{routeName}</div>
+	<div class="text-gray-600">
+		{#if predicted}
+			Vehicle #<span class="font-semibold text-blue-500">{vehicleId || 'N/A'}</span> | Data updated
+			<span class="font-semibold text-blue-500">{formatLastUpdated(lastUpdateTime)}</span>
+		{:else}
+			<span class="font-semibold text-gray-500"
+				>we don't have a real-time data for this vehicle now.</span
+			>
+		{/if}
 	</div>
 	<br />
-	<div style="font-weight: bold;">Next stop:</div>
-	<div style="padding-left: 20px;">
-		<strong>{nextStopName || 'N/A'}</strong>
+	<div class="font-bold text-gray-800">Next stop:</div>
+	<div class="text-gray-600">
+		<strong class="font-semibold text-blue-500">{nextStopName || 'N/A'}</strong>
 	</div>
 	<br />
-	<div style="font-weight: bold;">Status:</div>
-	<div style="padding-left: 20px;">
-		<strong>{status || 'N/A'}</strong>
+	<div class="font-bold text-gray-800">Status:</div>
+	<div class="text-gray-600">
+		<strong class="font-semibold text-blue-500">{status || 'N/A'}</strong>
 	</div>
 </div>
