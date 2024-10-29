d83433f0345bc2a0c8c6f7cba2889e8b55df46ac
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-10-29 06:14:47 +0300

    refactor: display color logic

--- src/components/RouteItem.svelte
+++ src/components/RouteItem.svelte
@@ -18,7 +18,7 @@
 	}
 
 	function getTextColor() {
-		return route.color === '#FFFFFF' || route.color === '#ffffff' ? '#000000' : route.color;
+		return route.color === 'FFFFFF' || route.color === 'ffffff' ? '000000' : route.color;
 	}
 </script>
 
@@ -27,7 +27,7 @@
 	class="route-item flex w-full items-center justify-between border-b border-gray-200 bg-[#f9f9f9] p-4 text-left hover:bg-[#e9e9e9] focus:outline-none dark:border-[#313135] dark:bg-[#1c1c1c] dark:text-white dark:hover:bg-[#363636]"
 	on:click={handleClick}
 >
-	<div class="text-lg font-semibold" style="color: #{getTextColor()}">
+	<div class="text-lg font-semibold" style="color: #{route.color}">
 		{getDisplayRouteName(route)}
 	</div>
 </button>
