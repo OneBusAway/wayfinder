a2d56e712430c988e79c5c57b9e95bb9e7f12b72
Bikram Ghuku <bikramghuku05@gmail.com>
2026-02-23 21:43:29 +0530

    feat: Test for forwardEvent called with no arguments, Consolidate repeated consoleSpy boilerplate, Use path alias in events.test.js import
    Signed-off-by: Bikram Ghuku <bikramghuku05@gmail.com>

--- src/routes/api/events/+server.js
+++ src/routes/api/events/+server.js
@@ -1,5 +1,6 @@
 import { PlausibleAnalytics } from '$lib/Analytics/PlausibleAnalytics.js';
 
+// Singleton: env values are read dynamically on each call via SvelteKit's env proxy
 const analytics = new PlausibleAnalytics();
 
 export async function POST({ request }) {
