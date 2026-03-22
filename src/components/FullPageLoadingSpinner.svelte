0b8af8affc6ae0a30cf1a927dccf6a03e3e92b53
Jason <jwu0592@gmail.com>
2026-03-21 20:50:26 -0400

    accessibility: Add ARIA attributes to loading spinner for better screen reader support

--- src/components/FullPageLoadingSpinner.svelte
+++ src/components/FullPageLoadingSpinner.svelte
@@ -3,6 +3,9 @@
 </script>
 
 <div
+	aria-label={$t('loading')}
+	role="status"
+	aria-live="polite"
 	class="flex h-full items-center justify-center bg-neutral-800 bg-gradient-to-br from-zinc-300 to-zinc-700 dark:from-zinc-500"
 >
 	<div class="flex items-center font-semibold text-white">
