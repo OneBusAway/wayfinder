a0056919f286739acf2ec1cf6e86a2c093e4d4bb
Hitanshi Goklani <hitanshigoklani33@gmail.com>
2026-03-30 04:16:35 +0530

    fix: Add preventDefault for Space key to prevent page scrolling

--- src/components/StopItem.svelte
+++ src/components/StopItem.svelte
@@ -24,7 +24,7 @@
 		tabindex="0"
 		class="stop-item dark:bg[#000000] flex w-full items-center justify-between border-b border-gray-200 bg-[#f9f9f9] p-4 text-left hover:bg-[#e9e9e9] focus:outline-none dark:border-[#313135] dark:bg-[#1c1c1c] dark:hover:bg-[#363636]"
 		onclick={() => handleStopItemClick(stop)}
-		onkeydown={(e) => { if (e.key === 'Enter' || e.key === ' ') handleStopItemClick(stop); }}
+		onkeydown={(e) => { if (e.key === 'Enter' || e.key === ' ') { if (e.key === ' ') e.preventDefault(); handleStopItemClick(stop); } }}
 	>
 		<div class="flex items-center gap-3 flex-1">
 			<div class="flex-1">
