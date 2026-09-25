01c0d5faaffc370526e3ca82feb3e9f28b9948b0
Ross Allen <rossgit@gmail.com>
2026-09-25 09:57:06 -0400

    Enable ESLint to parse and lint TypeScript
    ESLint must be configured to parse TypeScript in order to be able to
    lint it. Add the necessary
    [@typescript-eslint](https://typescript-eslint.io/) packages and
    configure them in order to suppport TS.
    * Convert `<StandalonePage>` to TypeScript to confirm TS in Svelte
      components is parsed correctly
    * Convert urls.js to TypeScript to confirm .ts files are parsed
      correctly

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,31 @@ import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import prettier from 'eslint-config-prettier';
 import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
 	js.configs.recommended,
+	...tsPlugin.configs['flat/recommended'].map((config) => ({
+		...config,
+		files: ['**/*.ts']
+	})),
+	...tsPlugin.configs['flat/stylistic'].map((config) => ({
+		...config,
+		files: ['**/*.ts']
+	})),
 	...svelte.configs['flat/recommended'],
 	prettier,
 	...svelte.configs['flat/prettier'],
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parserOptions: {
+				parser: { ts: tsParser }
+			}
+		}
+	},
 	{
 		languageOptions: {
 			globals: {
